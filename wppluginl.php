95d908902a851ae737ce5ac7c599fac034541c4b
ullakalaim8 <kaleemon.mohammed@gmail.com>
2017-04-13 15:47:43 +0530

    Update wppluginl.php

--- wppluginl.php
+++ wppluginl.php
@@ -1,17 +1,19 @@
 <?php
-/**
- * Plugin Name: testindian
- * Plugin URI: https://www.intellasphere.com/
- * Description: Intellasphere toolkit that helps you Integrate Intellasphere. Beautifully.
- * Version: 1.0.0
- * Author: Intellasphere
- * Author URI: https://www.intellasphere.com/
- * Requires at least: 4.4
- * Text Domain: Intellasphere
- * @package Intellasphere
- * @category Core
- * @author Intellasphere
- * GitHub Plugin URI: https://github.com/ullakalaim8/wppluginl
- */
- 
- echo "welcome";
+/*
+	Plugin Name: Smashing Plugin
+	Description: This is for updating your Wordpress plugin.
+	Version: 1.0.0
+	Author: Matthew Ray
+	Author URI: http://www.matthewray.com
+*/
+if( ! class_exists( 'Smashing_Updater' ) ){
+	include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
+}
+
+$updater = new Smashing_Updater( __FILE__ );
+$updater->set_username( 'rayman813' );
+$updater->set_repository( 'smashing-updater-plugin' );
+/*
+	$updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
+*/
+$updater->initialize();
