a5689d1b29fc24621df613a1ed78420186ef6888
ullakalaim8 <kaleemon.mohammed@gmail.com>
2017-04-13 15:23:36 +0530

    firscomment
    firscomment

---
+++ wppluginl.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: testindian
+ * Plugin URI: https://www.intellasphere.com/
+ * Description: Intellasphere toolkit that helps you Integrate Intellasphere. Beautifully.
+ * Version: 1.0.0
+ * Author: Intellasphere
+ * Author URI: https://www.intellasphere.com/
+ * Requires at least: 4.4
+ * Text Domain: Intellasphere
+ * @package Intellasphere
+ * @category Core
+ * @author Intellasphere
+ */
+ 
+ echo "welcome";
